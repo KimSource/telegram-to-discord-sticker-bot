d08d83a8eb0dd777bceb26cfc83aa528dadb89b1
KimSource <zxcv9959.kim@gmail.com>
2021-07-08 23:38:13 +0900

    telegraf 추가

---
+++ src/app.ts
@@ -0,0 +1,66 @@
+import 'dotenv/config';
+import { Context, Telegraf } from 'telegraf';
+
+async function fetchFile(ctx: Context, fileId: string): Promise<any> {
+  const fileLink = await ctx.telegram.getFileLink(fileId);
+  // TODO: HTTP 요청
+  return;
+}
+
+export function launchBot(token: string): void {
+  const bot = new Telegraf(token);
+
+  bot.use(async (ctx, next) => {
+    console.time(`Processing update ${ctx.update.update_id}`);
+    await next();
+    console.timeEnd(`Processing update ${ctx.update.update_id}`);
+  });
+
+  bot.command('start', async (ctx, next) => {
+    await ctx.reply('Send me a sticker to use on Discord');
+    return next();
+  });
+
+  bot.on('message', async (ctx, next) => {
+    if ('sticker' in ctx.message) {
+      const replyMessage = await ctx.reply('Downloading file');
+      const fileId = ctx.message.sticker.file_id;
+
+      try {
+        await fetchFile(ctx, fileId).then(
+          (stickerFile) => {
+            // TODO: 변환
+            console.log(stickerFile);
+            return stickerFile;
+          },
+          (err) => {
+            console.error(err);
+            throw new Error('Failed to fetch the sticker');
+          },
+        ).then(
+          (stickerFile) => {
+            // TODO: 파일 전송
+            // ctx.replyWithDocument(stickerFile);
+          },
+          (err) => {
+            console.error(err);
+            throw new Error('Failed to convert the sticker');
+          }
+        );
+        await ctx.telegram.deleteMessage(replyMessage.chat.id, replyMessage.message_id);
+      } catch (err) {
+        await ctx.telegram.editMessageText(replyMessage.chat.id, replyMessage.message_id, undefined, err?.message || 'Unknown error');
+      }
+    } else {
+      await ctx.reply('Send me a sticker to use on Discord');
+    }
+    return next();
+  });
+
+  bot.launch().then(() => {
+    console.log('Telegram bot launched');
+  });
+
+  process.once('SIGINT', () => bot.stop('SIGINT'));
+  process.once('SIGTERM', () => bot.stop('SIGTERM'));
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello, world!');
+import { launchBot } from './app';
+
+if (require.main === module) {
+  const token = process.env.BOT_TOKEN;
+
+  if (token) {
+    launchBot(token);
+  } else {
+    console.error('BOT_TOKEN is empty');
+  }
+}
+
+export { launchBot as launchApp };
